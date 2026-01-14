e0a23c6742fd9b5e81369e47b1c8884e2248e56c
timkroeger <TKroeger@gasag.de>
2026-01-14 08:56:22 +0100

    Name adaption

--- Confluence/Install/buildGERMA.sh
+++ Confluence/Install/buildGERMA.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo docker build -f DOCKERFILE --no-cache -t germag:live .
+sudo docker build -f DOCKERFILE --no-cache -t germa:live .
 read -r -p "Database Password: " dbPassword 
-sudo docker rm -f germag-live
+sudo docker rm -f germa-live
 sudo docker run \
   -d -p 8443:443 \
-  --name germag-live \
+  --name germa-live \
   --env DATABASE_CONNECTION="Host=172.19.0.50:5432;Database=germa;Username=postgres;Password=$dbPassword" \
   --restart always \
   -v /home/certs:/root/certs:ro \
